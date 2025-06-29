7bd55b2876e4a00b1b738d34dedf4b1b174effc0
lightningflashh <thanhchi0429@gmail.com>
2025-06-29 20:47:21 +0700

    main(service): test service, adding formatters

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,10 +1,11 @@
 import { StatusCodes } from 'http-status-codes'
+import { boardService } from '~/services/boardService'
 
 const createNew = async (req, res, next) => {
   try {
-    res.status(StatusCodes.CREATED)
-      .json({ message: 'Board created successfully from controller' })
+    const createdBoard = await boardService.createNew(req.body)
 
+    res.status(StatusCodes.CREATED).json(createdBoard)
   } catch (error) { next(error) }
 }
 
